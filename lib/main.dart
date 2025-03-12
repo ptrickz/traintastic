c4919c0c1fac80c3e31d7e234ca33e06d51d8085
Pattrick <pattrickpatt@gmail.com>
2025-03-13 01:41:11 +0800

    feat: configured firebase into the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:traintastic/core/utils/constants/colors.dart';
+import 'package:traintastic/firebase_options.dart';
 import 'package:traintastic/presentation/screens/auth/login.dart';
 import 'package:traintastic/presentation/screens/onboarding/onboarding.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
